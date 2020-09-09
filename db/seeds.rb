cc9c5d743a4f05cc354a4e4708eb3e19d3848c7f
佐藤　聖利 <ohayoupoteto20000202@ezweb.ne.jp>
2020-09-09 18:54:00 +0900

    2nd push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "管理人A", faculty: "教務課",admin: true,student_id: "adminA",password: "admin",password_confirmation: "admin",grade: 2)
+User.create!(name: "syamu_game", faculty: "ネットワーク情報学部", admin: false, student_id: "ne0",password: "0",password_confirmation: "0", grade: 3)
