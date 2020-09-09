2c4826cfc95bfda3388770f24ab2979c69960492
佐藤　聖利 <ohayoupoteto20000202@ezweb.ne.jp>
2020-09-09 18:54:00 +0900

    2nd push

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
     validates :name, presence: true ,uniqueness: true ,length: {minimum:1, maximum: 10 ,allow_blank: true}
     validates :faculty, presence: true ,length: {minimum:1, maximum: 10 ,allow_blank: true}
-    validates :grade, presence: true  ,numericality: {only_integer: true,greater_than: 1,less_than: 4,allow_blank: true}
+    validates :grade, presence: true  ,numericality: {only_integer: true,greater_than: 0,less_than: 5,allow_blank: true}
     validates :admin, presence: true
     validates :password, presence: true,length: {minimum:1, maximum: 10 ,allow_blank: true}
 end
