3397cef169303c571ef4bb16b26f1bae1309845c
佐藤　聖利 <ohayoupoteto20000202@ezweb.ne.jp>
2020-09-09 23:57:37 +0900

    リモートリポジトリ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'sessions#top'
-  get "top" => "sessions/top"
+  get "login"=> "sessions#login"
   
   get "q1" => "matters#q1"
   post "q1_after" => "matters#q1_after"
